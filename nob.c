ec9349a7004fd426d53ae024206f4a4781399c40
Jakob Kjær-Kammersgaard <jakob@miscellus.com>
2025-11-08 21:51:12 +0100

    fix tests

--- nob.c
+++ nob.c
@@ -2,52 +2,95 @@
 #define NOB_STRIP_PREFIX
 #include "nob.h"
 
-static Cmd cmd;
+static Cmd cmd_;
+static Cmd *cmd = &cmd_;
 
 #define SRC "src/"
 #define BUILD "build/"
+#define TESTS "tests/"
+#define RUNNERS TESTS "runners/"
+
+void append_cc(void)
+{
+    cmd_append(cmd, "cl");
+    cmd_append(cmd, "/nologo");
+    cmd_append(cmd, "/std:c11");
+    cmd_append(cmd, "/W4");
+    cmd_append(cmd, "/Od");
+    cmd_append(cmd, "/Zi");
+    cmd_append(cmd, "/D_CRT_SECURE_NO_WARNINGS");
+
+    // cmd_append(cmd, "/fsanitize=address");
+}
+
+void append_test(void)
+{
+    append_cc();
+    cmd_append(cmd, "/DUNITY_INCLUDE_DOUBLE");
+    cmd_append(cmd, TESTS "unity.c");
+
+}
 
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
-    if (!mkdir_if_not_exists(BUILD)) return 1;
+    bool make_calculator = false;
+    bool run = true;
+    bool make_tests = true;
 
-    if (true)
+    if (make_calculator)
     {
+        if (!mkdir_if_not_exists(BUILD)) return 1;
 
-        cmd_append(&cmd, "cl");
+        append_cc();
+        cmd_append(cmd, "/I..\\src");
+        cmd_append(cmd, SRC "calculator.c");
+        cmd_append(cmd, SRC "tokenizer.c");
+        cmd_append(cmd, SRC "parser.c");
 
-        cmd_append(&cmd, "/nologo");
-        cmd_append(&cmd, "/D_CRT_SECURE_NO_WARNINGS");
+        cmd_append(cmd, "/Fe:" BUILD "calculator.exe");
 
-        cmd_append(&cmd, "/W4");
-        cmd_append(&cmd, "/std:c11");
-        cmd_append(&cmd, "/Od");
-        cmd_append(&cmd, "/Zi");
+        if (!cmd_run(cmd)) return 1;
 
-        // cmd_append(&cmd, "/fsanitize=address");
-
-        cmd_append(&cmd, "/I..\\src");
-
-        cmd_append(&cmd, SRC "calculator.c");
-        cmd_append(&cmd, SRC "tokenizer.c");
-        cmd_append(&cmd, SRC "parser.c");
-
-        cmd_append(&cmd, "/Fe:" BUILD "calculator.exe");
-
-        if (!cmd_run(&cmd)) return 1;
+        if (run)
+        {
+            cmd_append(cmd, BUILD "calculator.exe");
+            cmd_append(cmd, "-print-infix");
+            // cmd_append(cmd, "-print-rpn");
+            // cmd_append(cmd, "-print-s");
+            cmd_append(cmd, "-input=(1 + 2*(3 - 4^0))/7 - 5^2");
+            if (!cmd_run(cmd)) return 1;
+        }
     }
 
-    if (false)
+    if (make_tests)
     {
-        cmd_append(&cmd, BUILD "calculator.exe");
-        // cmd_append(&cmd, "-print-infix");
-        // cmd_append(&cmd, "-print-rpn");
-        // cmd_append(&cmd, "-print-s");
-        cmd_append(&cmd, "-input=(1 + 2*(3 - 4^0))/7 - 5^2");
-        if (!cmd_run(&cmd)) return 1;
+        if (!mkdir_if_not_exists(RUNNERS)) return 1;
+
+        append_test();
+        cmd_append(cmd, SRC "tokenizer.c");
+        cmd_append(cmd, TESTS "test_tokenizer.c");
+        cmd_append(cmd, "/Fe:" RUNNERS "test_tokenizer.test.exe");
+        if (!cmd_run(cmd)) return 1;
+
+        append_test();
+        cmd_append(cmd, SRC "tokenizer.c");
+        cmd_append(cmd, SRC "parser.c");
+        cmd_append(cmd, TESTS "test_parser.c");
+        cmd_append(cmd, "/Fe:" RUNNERS "test_parser.test.exe");
+        if (!cmd_run(cmd)) return 1;
+
+        if (run)
+        {
+            cmd_append(cmd, RUNNERS "test_tokenizer.test.exe");
+            if (!cmd_run(cmd)) return 1;
+
+            cmd_append(cmd, RUNNERS "test_parser.test.exe");
+            if (!cmd_run(cmd)) return 1;
+        }
     }
+
 
     return 0;
 }
