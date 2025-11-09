0534f61075ea703d23aaf2828e46182d7ada916f
Jakob Kjær-Kammersgaard <jakob@miscellus.com>
2025-11-09 21:21:57 +0100

    .

--- nob.c
+++ nob.c
@@ -51,11 +51,26 @@ int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
-    bool make_calculator = true;
-    bool run = true;
-    bool make_tests = true;
+    const char *program = shift(argv, argc);
 
-    if (make_calculator)
+    bool build = true;
+    bool run = false;
+    bool test = false;
+
+    while (argc) {
+        char *arg = shift(argv, argc);
+
+        if (strcmp(arg, "test") == 0) {
+            build = false;
+            test = true;
+        }
+
+        if (strcmp(arg, "run") == 0) {
+            run = true;
+        }
+    }
+
+    if (build)
     {
         if (!mkdir_if_not_exists(BUILD)) return 1;
 
@@ -80,33 +95,46 @@ int main(int argc, char **argv)
         }
     }
 
-    if (make_tests)
+    if (test)
     {
         if (!mkdir_if_not_exists(RUNNERS)) return 1;
 
-        append_test();
-        cmd_append(cmd, SRC "tokenizer.c");
-        cmd_append(cmd, TESTS "test_tokenizer.c");
-        cmd_cc_output(RUNNERS "test_tokenizer.test.exe");
-        if (!cmd_run(cmd)) return 1;
+        const char *test_tokenizer_exe = RUNNERS "test_tokenizer.test.exe";
+        const char *test_parser_exe = RUNNERS "test_parser.test.exe";
 
-        append_test();
-        cmd_append(cmd, SRC "tokenizer.c");
-        cmd_append(cmd, SRC "parser.c");
-        cmd_append(cmd, TESTS "test_parser.c");
-        cmd_cc_output(RUNNERS "test_parser.test.exe");
-        cmd_append(cmd, "-lm");
+        static const char *test_input_paths[] = {
+            SRC "parser.c",
+            SRC "parser.h",
+            SRC "tokenizer.c",
+            SRC "tokenizer.h",
+            TESTS "test_parser.c",
+            TESTS "test_tokenizer.c",
+        };
 
-        if (!cmd_run(cmd)) return 1;
-
-        if (run)
+        if (needs_rebuild(test_tokenizer_exe, test_input_paths, ARRAY_LEN(test_input_paths)))
         {
-            cmd_append(cmd, RUNNERS "test_tokenizer.test.exe");
+            log(INFO, "Rebuilding tests");
+            append_test();
+            cmd_append(cmd, SRC "tokenizer.c");
+            cmd_append(cmd, TESTS "test_tokenizer.c");
+            cmd_cc_output(test_tokenizer_exe);
             if (!cmd_run(cmd)) return 1;
 
-            cmd_append(cmd, RUNNERS "test_parser.test.exe");
+            append_test();
+            cmd_append(cmd, SRC "tokenizer.c");
+            cmd_append(cmd, SRC "parser.c");
+            cmd_append(cmd, TESTS "test_parser.c");
+            cmd_cc_output(test_parser_exe);
+            cmd_append(cmd, "-lm");
+
             if (!cmd_run(cmd)) return 1;
         }
+
+        cmd_append(cmd, RUNNERS "test_tokenizer.test.exe");
+        if (!cmd_run(cmd)) return 1;
+
+        cmd_append(cmd, RUNNERS "test_parser.test.exe");
+        if (!cmd_run(cmd)) return 1;
     }
 
 
